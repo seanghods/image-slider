b47c7b439ebcf56ff58a78f2724bbf4f320eeef4
Sean Ghods <137848705+seanghods@users.noreply.github.com>
2023-08-16 11:46:40 -0700

    Create next previous button, create navigate circles

--- script.js
+++ script.js
@@ -1,21 +1,60 @@
 const slides = document.querySelectorAll(".slide");
+const boxes = document.querySelectorAll(".box");
 let currentSlide = 0;
-const slideInterval = setInterval(nextSlide, 2000);
+let slideInterval = setInterval(nextSlide, 2000);
 
 function nextSlide() {
+  clearInterval(slideInterval);
   slides[currentSlide].className = "slide";
-  currentSlide++;
-  if (currentSlide > slides.length - 1) currentSlide = 0;
+  changeOldCircleColor();
+  currentSlide == slides.length - 1 ? (currentSlide = 0) : currentSlide++;
   slides[currentSlide].className = "slide active";
+  changeNewCircleColor();
+  slideInterval = setInterval(nextSlide, 2000);
 }
 
 function previousSlide() {
+  clearInterval(slideInterval);
   slides[currentSlide].className = "slide";
+  changeOldCircleColor();
   currentSlide == 0 ? (currentSlide = slides.length - 1) : currentSlide--;
   slides[currentSlide].className = "slide active";
+  changeNewCircleColor();
+  slideInterval = setInterval(nextSlide, 2000);
 }
 
-const next = document.querySelector("#next");
-const previous = document.querySelector("#previous");
-next.addEventListener("click", nextSlide);
-previous.addEventListener("click", previousSlide);
+function addButtonFunc() {
+  const next = document.querySelector("#next");
+  const previous = document.querySelector("#previous");
+  next.addEventListener("click", nextSlide);
+  previous.addEventListener("click", previousSlide);
+
+  const boxes = document.querySelectorAll(".box");
+  for (const each of boxes) {
+    each.addEventListener("click", (e) => {
+      clearInterval(slideInterval);
+      slides[currentSlide].className = "slide";
+      changeOldCircleColor();
+      currentSlide = e.currentTarget.id;
+      changeNewCircleColor();
+      slides[currentSlide].className = "slide active";
+      slideInterval = setInterval(nextSlide, 2000);
+    });
+  }
+}
+
+function changeOldCircleColor() {
+  const filled = boxes[currentSlide].querySelector(`.fill`);
+  filled.setAttribute("display", "none");
+  const unfilled = boxes[currentSlide].querySelector(`.unfill`);
+  unfilled.setAttribute("display", "block");
+}
+
+function changeNewCircleColor() {
+  const filled = boxes[currentSlide].querySelector(`.fill`);
+  filled.setAttribute("display", "block");
+  const unfilled = boxes[currentSlide].querySelector(`.unfill`);
+  unfilled.setAttribute("display", "none");
+}
+
+addButtonFunc();
